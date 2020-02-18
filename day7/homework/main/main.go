d2102b9509c5430af22a6d42ac792d495a5c5e47
huruizhi <huruizhi@pystandard.com>
2020-02-19 01:03:04 +0800

    homework 思路整理

--- day7/homework/main/main.go
+++ day7/homework/main/main.go
@@ -15,3 +15,12 @@ func main() {
 	var mysqlConfig MysqlConnection
 	go_ini.ParseIni("./config.ini", mysqlConfig)
 }
+
+// 0. 参数的校验
+// 0.1 传进来的data 必须是指针类型
+// 0.2 传进来的data 必须是结构体类型
+// 1. 读取文件得到字节类型数据
+// 2. 一行一行读取数据
+// 2.1 注释的话跳过
+// 2.2 如果是[ 开头表示是节
+// 2.3 如果不是[开头 用= 进行分割
